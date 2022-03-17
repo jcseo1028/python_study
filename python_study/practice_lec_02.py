036fdcd1b3fdbbc47807ca6884fae40f9dcb6b5c
jcseo <jcseo1028@gmail.com>
2022-03-17 16:42:37 +0900

    re #1 2강 학습 완료.

---
+++ python_study/practice_lec_02.py
@@ -0,0 +1,41 @@
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋㅋㅋㅋ")
+# print("ㅋ"*7)
+
+# print(5>10)
+# print(5<10)
+
+# print(True)
+# print(False)
+
+# print(not False)
+# print(not True)
+
+# 변수
+# animal = "강아지"
+# name = "연탄이"
+# age = 4
+# hobby = "산책"
+# is_Adult = age >= 3
+
+# print ("우리집 " + animal + "의 이름은 " + name + "에요.")
+# #print(name + "은/는 " + str(age) + " 살이고, " + hobby + "을/를 아주 좋아해요.")
+# print(name + "은/는 " , age ," 살이고, " , hobby , "을/를 아주 좋아해요.") #, 에 빈칸이 하나씩 들어감. ,일 때는 str() 없이 변수만 사용해도 됨.
+# #여러 문장의 주석은 Ctrl+/ 
+# print(name + "은/는 어른일까요? " + str(is_Adult))
+
+#Quiz
+# station = "사당"
+# station = "신도림"
+station = "인천공항"
+
+print(station + "행 열차가 들어오고 있습니다.")
