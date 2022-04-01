f02d9f3506fa594069f68faae4d07b7483889b77
jcseo <jcseo1028@gmail.com>
2022-04-01 18:17:02 +0900

    re #7 기본편 8강 학습중...

--- python_study/practice_lec_08.py
+++ python_study/practice_lec_08.py
@@ -10,11 +10,16 @@
 # print("Python", "Java", file=sys.stdout) # 표준 출력
 # print("Python", "Java", file=sys.stderr) # 표준 에러
 
-scores = {"수학":70, "영어":50, "코딩":100} # dictionary , 
-for subject, score in scores.items():
-    print(subject + " : " + str(score))
-    print(subject.ljust(8), )
+# scores = {"수학":70, "영어":50, "코딩":100} # dictionary , 
+# for subject, score in scores.items():
+#     # print(subject + " : " + str(score))
+#     print(subject.ljust(8), str(score).rjust(4), sep=":") # ljust : 왼쪽 정렬, rjust : 오른쪽 정렬
 
+# for num in range(1, 21):
+#     print("대기번호 : " + str(num).zfill(3))
+
+# answer = input("아무 값이나 입력하시오 : ")
+# print("입력값 : " + answer) # 숫자도 string type 으로 입력된다.
 
 
 # 다양한 출력 포맷
