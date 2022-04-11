d5a8acd6f74f9ef53dcb74d464ea4170bf856d89
jcseo <jcseo1028@gmail.com>
2022-04-11 19:28:58 +0900

    re #9 기본편 10강 학습 완료

--- python_study/practice_lec_10.py
+++ python_study/practice_lec_10.py
@@ -1,37 +1,112 @@
 
 # 예외 처리
 
-try:
-    print("나누기 전용 계산기입니다.")
-    nums = []
-    nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
-    nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
-    nums.append(int(nums[0] / nums[1])) # 계산 결과를 리스트에 추가
-    print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
-except ValueError:
-    print("에러! 잘못된 값을 입력하였습니다.")
-except ZeroDivisionError as err:
-    print(err)
-except Exception as err:
-    print("알 수 없는 에러가 발생하였습니다.")
-    print(err)
-
-
-
-
+# try:
+#     print("나누기 전용 계산기입니다.")
+#     nums = []
+#     nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
+#     nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
+#     nums.append(int(nums[0] / nums[1])) # 계산 결과를 리스트에 추가
+#     print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
+# except ValueError:
+#     print("에러! 잘못된 값을 입력하였습니다.")
+# except ZeroDivisionError as err:
+#     print(err)
+# except Exception as err:
+#     print("알 수 없는 에러가 발생하였습니다.")
+#     print(err)
 
 ##########################################################################################################
 
 # 에러 발생시키기
 
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요: "))
+#     num2 = int(input("두 번째 숫자를 입력하세요: "))
+#     if num1 >= 10 or num2 >= 10: # 입력받은 수가 한 자리인지 확인
+#         raise ValueError # 특정 조건에 Error 발생시키기.
+#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+
 ##########################################################################################################
 
 # 사용자 정의 예외 처리
+
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+
+#     def __str__(self):
+#         return "[에러코드 001] " + self.msg # 에러 메시지 가공
+
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요: "))
+#     num2 = int(input("두 번째 숫자를 입력하세요: "))
+#     if num1 >= 10 or num2 >= 10: # 입력받은 수가 한 자리인지 확인
+#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2)) # 자세한 에러 메시지
+#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+# except BigNumberError as err:
+#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")
+#     print(err) # 에러 메시지 출력
 
 ##########################################################################################################
 
 # Finally
 
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+
+#     def __str__(self):
+#         return self.msg
+
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요: "))
+#     num2 = int(input("두 번째 숫자를 입력하세요: "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
+#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+# except BigNumberError as err:
+#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")
+#     print(err)
+# finally: # 에러 발생 여부 상관 없이 항상 실행
+#     print("계산기를 이용해 주셔서 감사합니다.")
+
 ##########################################################################################################
 
 # Quiz
+
+class SoldOutError(Exception):
+    pass
+
+chicken = 10
+waiting = 1
+
+while(True):
+    try:
+        print("[남은 치킨 : {0}]".format(chicken))
+        order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+        if order > chicken:
+            print("재료가 부족합니다.")    
+        elif order <= 0:
+            raise ValueError    
+        else:
+            print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다.".format(waiting, order))
+            waiting += 1
+            chicken -= order
+
+        if chicken == 0:
+            raise SoldOutError
+    except ValueError:
+        print("잘못된 값을 입력하였습니다.")
+    except SoldOutError:
+        print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+        break
---
+++ python_study/test.md
@@ -0,0 +1,80 @@
+# H1 태그
+
+## H2 Tag
+
+### H3 Tag
+
+#### H4 Tag
+
+##### H5 Tag
+
+###### H6 Tag
+
+그냥 일반 글자.
+
+
+---
+
+_italic_
+
+**Bold**
+
+*seojincheol*
+
+~~Canceled~~
+
+> 인용문인가?
+
+---
+* list1
+* list2
+* list3
+
+
+- list1
+- list2
+- list3
+  - A
+    - B
+
+1. list1
+2. list2
+3. list3
+   1. A
+      1. B
+
+---
+
+|제목|description|비고|
+|-|-|-|
+|안녕|1|2|
+
+---
+
+[내 Github Page](https://jcseo1028.github.io)
+
+---
+
+`simple code`
+
+```csharp
+
+using System;
+using System.Data;
+
+public static voud Main()
+{
+    Console.WriteLine("dk");
+}
+
+```
+
+---
+
+
+
+
+
+
+
+
