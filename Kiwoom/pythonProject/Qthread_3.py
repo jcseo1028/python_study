d44503a50db9475818ff7cb85a2ca0de3fca9bd2
Jincheol, SEO <jcseo1028@gmail.com>
2024-10-24 21:04:08 +0900

    실시간 자동 매매 관련 수정 진행 중.

--- Kiwoom/pythonProject/Qthread_3.py
+++ Kiwoom/pythonProject/Qthread_3.py
@@ -20,6 +20,9 @@ class Thread3(QThread):
 
         ################# 매수관련 변수
         self.Load_code()    # 매수 종목/금액/수량 가져오기
+        self.orderitemlist_1 = []  # 중복 매수 금지
+        self.orderitemlist_2 = []  # 중복 익절 금지
+        self.orderitemlist_3 = []  # 중복 손절 금지
 
         ####### 주문 전송 시 필요한 FID 번호
         self.realType = RealType()  # 실시간 FID 번호를 모아두는 곳
@@ -40,6 +43,10 @@ class Thread3(QThread):
                                       "1")  # 실시간 데이터를 받아오기 위해 각 코드들을 서버에 등록(틱 변화가 있으면 데이터 송신)
             self.screen_num += 1
 
+        # print("실시간 등록 : %s, 스크린번호 : %s, FID  번호 : %s" % (code, screen_num, fids))
+        print("종목등록 완료")
+        print(self.k.portfolio_stock_dict.keys())
+
         ######################################################################
         ###### 현재 장 상태 알아보기 (장 시작 / 장 마감 등)
         self.screen_start_stop_real = "300"  # 장시 시작 전/후 상태 확인용 스크린 번호
@@ -47,6 +54,7 @@ class Thread3(QThread):
 
         ###### 실시간 슬롯 (데이터를 받아오는 슬롯을 설정한다)
         self.k.kiwoom.OnReceiveRealData.connect(self.realdata_slot)  # 실시간 데이터를 받아오는 곳
+        self.k.kiwoom.OnReceiveChejanData.connect(self.chejan_slot)  # (주문접수, 체결통보)=0, (잔고변경) = 1 데이터 전송
 
     def Load_code(self):
 
@@ -101,3 +109,264 @@ class Thread3(QThread):
 
             elif value == '4':
                 print("장 마감했습니다.")
+
+        elif sRealType == "주식체결" and sCode in self.k.portfolio_stock_dict:
+            fid1 = self.realType.REALTYPE[sRealType]['체결시간']  # 체결시간은 string으로 나온다. HHMMSS
+            a = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid1)
+
+            fid2 = self.realType.REALTYPE[sRealType]['현재가']  # 현재가는 +/-로 나온다.
+            b = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid2)
+            b = abs(int(b))
+
+            fid3 = self.realType.REALTYPE[sRealType]['전일대비']  # 전달 대비 오르거나/내린 가격
+            c = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid3)
+            c = abs(int(c))
+
+            fid4 = self.realType.REALTYPE[sRealType]['등락율']  # 전달 대비 오르거나/내린 퍼센테이지
+            d = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid4)
+            d = float(d)
+
+            fid5 = self.realType.REALTYPE[sRealType]['(최우선)매도호가']  # 매도쪽에 첫번재 부분(시장가)
+            e = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid5)
+            e = abs(int(e))
+
+            fid6 = self.realType.REALTYPE[sRealType]['(최우선)매수호가']  # 매수쪽에 첫번재 부분(시장가)
+            f = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid6)
+            f = abs(int(f))
+
+            fid7 = self.realType.REALTYPE[sRealType]['거래량']  # 틱봉의 현재 거래량 (아주 작으 단위)
+            g = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid7)
+            g = abs(int(g))
+
+            fid8 = self.realType.REALTYPE[sRealType]['누적거래량']
+            h = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid8)
+            h = abs(int(h))
+
+            fid9 = self.realType.REALTYPE[sRealType]['고가']  # 오늘자 재일 높은 가격
+            i = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid9)
+            i = abs(int(i))
+
+            fid10 = self.realType.REALTYPE[sRealType]['시가']  # 시가
+            j = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid10)
+            j = abs(int(j))
+
+            fid11 = self.realType.REALTYPE[sRealType]['저가']  # 전체 재일 낮은 가격
+            k = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid11)
+            k = abs(int(k))
+
+            fid12 = self.realType.REALTYPE[sRealType]['거래회전율']  # 누적 거래회전율
+            l = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid12)
+            l = abs(float(l))
+
+            if sCode not in self.k.portfolio_stock_dict:  # 만약 서버에 등록된 코드가 포트폴리오에 없다면 코드를 등록
+                self.k.portfolio_stock_dict.update({sCode: {}})
+
+                # 포트폴리오 종목코드마다 아래 실시간 데이터를 입력
+            self.k.portfolio_stock_dict[sCode].update({"채결시간": a})  # 아래 내용을 업데이트
+            self.k.portfolio_stock_dict[sCode].update({"현재가": b})
+            self.k.portfolio_stock_dict[sCode].update({"전일대비": c})
+            self.k.portfolio_stock_dict[sCode].update({"등락율": d})
+            self.k.portfolio_stock_dict[sCode].update({"(최우선)매도호가": e})
+            self.k.portfolio_stock_dict[sCode].update({"(최우선)매수호가": f})
+            self.k.portfolio_stock_dict[sCode].update({"거래량": g})
+            self.k.portfolio_stock_dict[sCode].update({"누적거래량": h})
+            self.k.portfolio_stock_dict[sCode].update({"고가": i})
+            self.k.portfolio_stock_dict[sCode].update({"시가": j})
+            self.k.portfolio_stock_dict[sCode].update({"저가": k})
+            self.k.portfolio_stock_dict[sCode].update({"거래회전율": l})
+
+            ###############################################################
+            ############# 실시간을 위한 조건문 구성하기 ########################
+            ###############################################################
+
+            # 1. 매수 알고리즘 가동
+
+            # 1차#############################################################################################
+            if self.k.portfolio_stock_dict[sCode]["현재가"] <= self.k.portfolio_stock_dict[sCode]["매수가"]:
+                if sCode not in self.orderitemlist_1:
+
+                    wa = []
+                    wa.append(sCode)
+
+                    if len(wa) > 1:
+                        wa.clear()
+                        pass
+                    else:
+                        print("매수 시작 %s" % sCode)
+
+                        self.orderitemlist_1.append(sCode)  # 이 기법을 더이상 사용하지 못하게 하기
+                        order_success1 = self.k.kiwoom.dynamicCall(
+                            "SendOrder(QString, QString, QString ,int, QString, int, int, QString, QString)",
+                            ["신규매수", self.k.portfolio_stock_dict[sCode]['주문용스크린번호'], self.account_num, 1, sCode,
+                             self.k.portfolio_stock_dict[sCode]["매수수량"], self.k.portfolio_stock_dict[sCode]["현재가"],
+                             self.realType.SENDTYPE['거래구분']['지정가'], ""])
+
+                        wf2 = open("dist/mesu_database.txt", "a",
+                                   encoding="utf8")  # "a" 달아 쓴다. "w" 덮어 쓴다. files라느 파이썬 페키지 볼더를 만든다.
+                        wf2.write("%s\t%s\t%s\t%s\n" % ("1매수정보", self.k.portfolio_stock_dict[sCode]["종목명"], b,
+                                                        self.k.portfolio_stock_dict[sCode]["채결시간"]))  # t는 tap을 의미한다.
+                        wf2.close()
+
+                        if order_success1 == 0:
+                            print("최우선매수호가로 주문 전달 성공")
+                        else:
+                            print("최우선매수호가로 주문 전달 실패")
+
+                        # 2. 매도 알고리즘 가동
+
+                        # 1차 익절 #############################################################################################
+                        if self.k.portfolio_stock_dict[sCode]["현재가"] >= self.k.portfolio_stock_dict[sCode]["익절가"]:
+                            if sCode not in self.orderitemlist_2:
+
+                                wa = []
+                                wa.append(sCode)
+
+                                if len(wa) > 1:
+                                    wa.clear()
+                                    pass
+                                else:
+                                    print("익절 시작 %s" % sCode)
+
+                                    self.orderitemlist_2.append(sCode)  # 이 기법을 더이상 사용하지 못하게 하기
+                                    order_success2 = self.k.kiwoom.dynamicCall(
+                                        "SendOrder(QString, QString, QString ,int, QString, int, int, QString, QString)",
+                                        ["신규익절", self.k.portfolio_stock_dict[sCode]['주문용스크린번호'], self.account_num, 2,
+                                         sCode,
+                                         self.k.portfolio_stock_dict[sCode]["매수수량"],
+                                         self.k.portfolio_stock_dict[sCode]["현재가"],
+                                         self.realType.SENDTYPE['거래구분']['지정가'], ""])
+
+                                    wf2 = open("dist/mesu_database.txt", "a",
+                                               encoding="utf8")  # "a" 달아 쓴다. "w" 덮어 쓴다. files라느 파이썬 페키지 볼더를 만든다.
+                                    wf2.write("%s\t%s\t%s\t%s\n" % (
+                                    "1익절정보", self.k.portfolio_stock_dict[sCode]["종목명"], b,
+                                    self.k.portfolio_stock_dict[sCode]["채결시간"]))  # t는 tap을 의미한다.
+                                    wf2.close()
+
+                                    if order_success2 == 0:
+                                        print("익절가로 주문 전달 성공")
+                                    else:
+                                        print("익절가로 주문 전달 실패")
+
+            # 2. 매도 알고리즘 가동
+
+            # 1차 익절 #############################################################################################
+            if self.k.portfolio_stock_dict[sCode]["현재가"] >= self.k.portfolio_stock_dict[sCode]["익절가"]:
+                if sCode not in self.orderitemlist_2:
+
+                    wa = []
+                    wa.append(sCode)
+
+                    if len(wa) > 1:
+                        wa.clear()
+                        pass
+                    else:
+                        print("익절 시작 %s" % sCode)
+
+                        self.orderitemlist_2.append(sCode)  # 이 기법을 더이상 사용하지 못하게 하기
+                        order_success2 = self.k.kiwoom.dynamicCall(
+                                        "SendOrder(QString, QString, QString ,int, QString, int, int, QString, QString)",
+                                        ["신규익절", self.k.portfolio_stock_dict[sCode]['주문용스크린번호'], self.account_num, 2,
+                                         sCode,
+                        self.k.portfolio_stock_dict[sCode]["매수수량"],
+                        self.k.portfolio_stock_dict[sCode]["현재가"],
+                        self.realType.SENDTYPE['거래구분']['지정가'], ""])
+
+                        wf2 = open("dist/mesu_database.txt", "a",
+                                               encoding="utf8")  # "a" 달아 쓴다. "w" 덮어 쓴다. files라느 파이썬 페키지 볼더를 만든다.
+                        wf2.write("%s\t%s\t%s\t%s\n" % (
+                                    "1익절정보", self.k.portfolio_stock_dict[sCode]["종목명"], b,
+                        self.k.portfolio_stock_dict[sCode]["채결시간"]))  # t는 tap을 의미한다.
+                        wf2.close()
+
+                        if order_success2 == 0:
+                            print("익절가로 주문 전달 성공")
+                        else:
+                            print("익절가로 주문 전달 실패")
+
+            # 1차 손절 #############################################################################################
+            if self.k.portfolio_stock_dict[sCode]["현재가"] <= self.k.portfolio_stock_dict[sCode]["손절가"]:
+                if sCode not in self.orderitemlist_3:
+
+                    wa = []
+                    wa.append(sCode)
+
+                    if len(wa) > 1:
+                         wa.clear()
+                         pass
+                    else:
+                         print("손절 시작 %s" % sCode)
+
+                         self.orderitemlist_3.append(sCode)  # 이 기법을 더이상 사용하지 못하게 하기
+                         order_success3 = self.k.kiwoom.dynamicCall(
+                                        "SendOrder(QString, QString, QString ,int, QString, int, int, QString, QString)",
+                                        ["신규손절", self.k.portfolio_stock_dict[sCode]['주문용스크린번호'], self.account_num, 2,
+                                         sCode,
+                                         self.k.portfolio_stock_dict[sCode]["매수수량"],
+                                         self.k.portfolio_stock_dict[sCode]["현재가"],
+                                         self.realType.SENDTYPE['거래구분']['지정가'], ""])
+
+                         wf2 = open("dist/mesu_database.txt", "a",
+                                               encoding="utf8")  # "a" 달아 쓴다. "w" 덮어 쓴다. files라느 파이썬 페키지 볼더를 만든다.
+                         wf2.write("%s\t%s\t%s\t%s\n" % (
+                                    "1손절정보", self.k.portfolio_stock_dict[sCode]["종목명"], b,
+                                    self.k.portfolio_stock_dict[sCode]["채결시간"]))  # t는 tap을 의미한다.
+                         wf2.close()
+
+                         if order_success3 == 0:
+                            print("손절가로 주문 전달 성공")
+                         else:
+                            print("손절가로 주문 전달 실패")
+
+    def chejan_slot(self, sGubun, nItemCnt, sFIdList):  # 주문전송 후 주문접수, 체결통보, 잔고통보를 수신
+
+        if sGubun == "0":
+            print("매수/매도 중입니다. 미체결 잔고 업데이트")
+        else:
+            print("미체결잔고 해결로 실제 잔고 업데이트")
+
+        if int(sGubun) == 0:  # 주문전송 후 미체결 되었을 때 아래와 같은 연산을 해 준다.
+            account_num = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['계좌번호'])
+            sCode = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['종목코드'])[
+                    1:]  # [A203042]
+            stock_name = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['종목명'])
+            stock_name = stock_name.strip()  # 혹시라도 공백이 있을 까봐
+            origin_order_number = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결'][
+                '원주문번호'])  # 원주문번호가 없으면 0000000이다
+            order_number = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['주문번호'])
+            order_status = self.k.kiwoom.dynamicCall("GetChejanData(int)",
+                                                     self.realType.REALTYPE['주문체결']['주문상태'])  # 접수/확인/체결 정보
+            order_quan = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['주문수량'])
+            order_quan = int(order_quan)
+            order_price = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['주문가격'])
+            order_price = int(order_price)
+            not_chegual_quan = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['미체결수량'])
+            not_chegual_quan = int(not_chegual_quan)
+            order_gubun = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결'][
+                '주문구분'])  # 정정 등, 부호가 나오기 때문에 잡아줘야 된다.
+            order_gubun = order_gubun.lstrip('+').lstrip('-')
+            order_gubun = order_gubun.strip()
+            chegual_time_str = self.k.kiwoom.dynamicCall("GetChejanData(int)",
+                                                         self.realType.REALTYPE['주문체결']['주문/체결시간'])  # '151028'
+            chegual_price = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['체결가'])
+
+            if chegual_price == '':
+                chegual_price = 0  # 숫자로 할당
+            else:
+                chegual_price = int(chegual_price)
+
+            chegual_quantity = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['체결량'])
+
+            if chegual_quantity == '':
+                chegual_quantity = 0
+            else:
+                chegual_quantity = int(chegual_quantity)
+
+            current_price = self.k.kiwoom.dynamicCall("GetChejanData(int)", self.realType.REALTYPE['주문체결']['현재가'])
+            current_price = abs(int(current_price))
+            first_sell_price = self.k.kiwoom.dynamicCall("GetChejanData(int)",
+                                                         self.realType.REALTYPE['주문체결']['(최우선)매도호가'])
+            first_sell_price = abs(int(first_sell_price))
+            first_buy_price = self.k.kiwoom.dynamicCall("GetChejanData(int)",
+                                                        self.realType.REALTYPE['주문체결']['(최우선)매수호가'])
+            first_buy_price = abs(int(first_buy_price))
--- Kiwoom/pythonProject/kiwoom.py
+++ Kiwoom/pythonProject/kiwoom.py
@@ -11,8 +11,15 @@ class Kiwoom(QWidget, metaclass=Singleton):
         self.kiwoom = QAxWidget('KHOPENAPI.KHOpenAPICtrl.1')
 
         ########## 전체 공유 데이터
-        self.All_Stock_Code = {}  # 코스피, 코스닥 전체 코드넘버 입력
-        self.acc_portfolio = {}  # 계좌에 들어있는 종목의 코드, 수익률 등등 입력
+        self.All_Stock_Code = {}            # 코스피, 코스닥 전체 코드넘버 입력
+        self.acc_portfolio = {}             # 계좌에 들어있는 종목의 코드, 수익률 등등 입력
+        self.portfolio_stock_dict = {}      # 매매에 관한 모든 종목(현재계좌 종목.금일 등록 종목)이 모두 들어간다.
 
-        self.portfolio_stock_dict = {}
+        self.today_meme = {}                # 금일 매매하는 종목에 대하여 들어간다.
+        self.not_account_stock_dict = {}    # 미체결 잔고
+
+        ########## 금일 잔고
+        self.jango_dict = {}                #
+        self.buy_jogon = {}                 # 미체결 잔고
+
 
